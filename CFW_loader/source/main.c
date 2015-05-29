9b9350c23ce1f56d3877a382dc74911e6cd2752e
luxsie <luxsie@163.com>
2015-05-29 21:19:45 +0800

    read main.c. cast some addresses with intent.

--- CFW_loader/source/main.c
+++ CFW_loader/source/main.c
@@ -8,158 +8,132 @@
 #include "draw.h"
 
 //VARIABLES
-char* systemVersion;
-char type;
+char* cfw_FWString;
+char cfw_FWValue;
 void ioDelay(u32);
 bool dump_arm9;
-
-void wait_key() {
+// @breif  [Unused]Simply wait for a key, but not use its value.
+// @note   Work like a pause.
+void CFW_WaitKey() {
 	DrawDebug(1,"");
 	DrawDebug(1,"Press key to continue");
 	HidWaitForInput();
 }
-
-void getSystemVersion()
-{
+// @breif  This reads the system version from the configuration file.
+// @note   So please make sure you've have the file in your SD.
+void CFW_getSystemVersion(void) {
 	char settings[16];
 	if (FSFileOpen("/3ds/PastaCFW/system.txt")){
 		FSFileRead(settings, 16, 0);
 		FSFileClose();
 	}
-
-	switch (settings[0])
-	{
-	case '1': // 4.x
-		type = '1';
-		systemVersion = "Old 3DS V. 4.1 - 4.5";
-		break;
-	case '2': // 5.0
-		type = '2';
-		systemVersion = "Old 3DS V. 5.0";
-		break;
-	case '3': // 5.1
-		type = '3';
-		systemVersion = "Old 3DS V. 5.1";
-		break;
-	case '4': // 6.0
-		type = '4';
-		systemVersion = "Old 3DS V. 6.0";
-		break;
-	case '5': // 6.1 - 6.3
-		type = '5';
-		systemVersion = "Old 3DS V. 6.1 - 6.3";
-		break;
-	case '6': // 7.0-7.1
-		type = '6';
-		systemVersion = "Old 3DS V. 7.0 - 7.1";
-		break;
-	case '7': // 7.2
-		type = '7';
-		systemVersion = "Old 3DS V. 7.2";
-		break;
-	case '8': // 8.x
-		type = '8';
-		systemVersion = "Old 3DS V. 8.0 - 8.1";
-		break;
-	case '9': // 9.x
-		type = '9';
-		systemVersion = "Old 3DS V. 9.0 - 9.2";
-		break;
-	case 'a': // 8.x
-		type = 'a';
-		systemVersion = "New 3DS V. 8.1";
-		break;
-	case 'b': // 9.x
-		type = 'b';
-		systemVersion = "New 3DS V. 9.0 - 9.2";
-		break;
+    cfw_FWValue = settings[0];
+	switch (settings[0]) {
+        //Old 3ds
+        case '1': //4.x
+            cfw_FWString = "Old 3DS V. 4.1 - 4.5";
+		    break;
+        case '2': // 5.0
+    		cfw_FWString = "Old 3DS V. 5.0";
+    		break;
+    	case '3': // 5.1
+    		cfw_FWString = "Old 3DS V. 5.1";
+    		break;
+    	case '4': // 6.0
+    		cfw_FWString = "Old 3DS V. 6.0";
+    		break;
+    	case '5': // 6.1 - 6.3
+    		cfw_FWString = "Old 3DS V. 6.1 - 6.3";
+    		break;
+    	case '6': // 7.0-7.1
+    		cfw_FWString = "Old 3DS V. 7.0 - 7.1";
+    		break;
+    	case '7': // 7.2
+    		cfw_FWString = "Old 3DS V. 7.2";
+    		break;
+    	case '8': // 8.x
+    		cfw_FWString = "Old 3DS V. 8.0 - 8.1";
+    		break;
+    	case '9': // 9.x
+    		cfw_FWString = "Old 3DS V. 9.0 - 9.2";
+    		break;
+        //New 3ds
+    	case 'a': // 8.x
+    		cfw_FWString = "New 3DS V. 8.1";
+    		break;
+    	case 'b': // 9.x
+    		cfw_FWString = "New 3DS V. 9.0 - 9.2";
+    		break;
 	}
 
 	//Check if to use the ARM9 Dumper
 	if (settings[2] == '1') dump_arm9 = true;
 }
-
-void bootCFW_SecondStage()
-{
-	u8 patch[] = { 0x00, 0x20, 0x3B, 0xE0 };
-	u8 patch1[] = { 0x00, 0x20, 0x08, 0xE0 };
-	u8 patch2[] = { 0x01, 0x27, 0x1E, 0xF5 };
-	u8 patch3[] = { 0xB4, 0xF9, 0xD0, 0xAB };
-	u8 patch4[] = { 0x6D, 0x20, 0xCE, 0x77 };
-	u8 patch5[] = { 0x5A, 0xC5, 0x73, 0xC1 };
+// @breif  Patch the offsets to pass the signs.
+// @note   So please make sure you've have the file in your SD.
+void CFW_SecondStage(void) {
+    //Old 3DS
+    u8 patchO0[] = { 0x00, 0x20, 0x3B, 0xE0 };
+    u8 patchO1[] = { 0x00, 0x20, 0x08, 0xE0 };
+    //New 3DS
+    u8 patchN0[] = { 0x01, 0x27, 0x1E, 0xF5 };
+    u8 patchN1[] = { 0xB4, 0xF9, 0xD0, 0xAB };
+    u8 patchN2[] = { 0x6D, 0x20, 0xCE, 0x77 };
+    u8 patchN3[] = { 0x5A, 0xC5, 0x73, 0xC1 };
 	//Apply patches
-	DrawDebug(0,"Apply patch for type %c...", type);
-	if (type == '1'){ // 4.x
-		u32 *dest = 0x080549C4;
-		u32 *dest1 = 0x0804239C;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '2'){ // 5.0
-		u32 *dest = 0x08051650;
-		u32 *dest1 = 0x0803C838;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '3'){ // 5.1
-		u32 *dest = 0x0805164C;
-		u32 *dest1 = 0x0803C838;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '4'){ // 6.0
-		u32 *dest = 0x0805235C;
-		u32 *dest1 = 0x08057FE4;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '5'){ // 6.1 - 6.3
-		u32 *dest = 0x08051B5C;
-		u32 *dest1 = 0x08057FE4;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '6'){ // 7.0-7.1
-		u32 *dest = 0x080521C4;
-		u32 *dest1 = 0x08057E98;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '7'){ // 7.2
-		u32 *dest = 0x080521C8;
-		u32 *dest1 = 0x08057E9C;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '8'){ // 8.x
-		u32 *dest = 0x080523C4;
-		u32 *dest1 = 0x08058098;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == '9'){ // 9.x
-		u32 *dest = 0x0805235C;
-		u32 *dest1 = 0x08058100;
-		memcpy(dest, patch, 4);
-		memcpy(dest1, patch1, 4);
-	}
-	else if (type == 'a'){ // 8.x
-		u32 *dest = 0x08053114;
-		u32 *dest1 = 0x080587E0;
-		memcpy(dest, patch2, 4);
-		memcpy(dest1, patch3, 4);
-	}
-	else if (type == 'b'){ // 9.x
-		u32 *dest = 0x08052FD8;
-		u32 *dest1 = 0x08058804;
-		memcpy(dest, patch4, 4);
-		memcpy(dest1, patch5, 4);
-	}
-	DrawDebug(1,"Apply patch for type %c...                  Done!", type);
+	DrawDebug(0,"Apply patch for type %c...", cfw_FWValue);
+    switch (cfw_FWValue) {
+        //Old 3ds
+        case '1': //4.x
+		    memcpy((u32*)0x080549C4, patchO0, 4);
+		    memcpy((u32*)0x0804239C, patchO1, 4);
+		    break;
+        case '2': // 5.0
+		    memcpy((u32*)0x08051650, patchO0, 4);
+		    memcpy((u32*)0x0803C838, patchO1, 4);
+    		break;
+    	case '3': // 5.1
+		    memcpy((u32*)0x0805164C, patchO0, 4);
+		    memcpy((u32*)0x0803C838, patchO1, 4);
+    		break;
+    	case '4': // 6.0
+		    memcpy((u32*)0x0805235C, patchO0, 4);
+		    memcpy((u32*)0x08057FE4, patchO1, 4);
+    		break;
+    	case '5': // 6.1 - 6.3
+		    memcpy((u32*)0x08051B5C, patchO0, 4);
+		    memcpy((u32*)0x08057FE4, patchO1, 4);
+    		break;
+    	case '6': // 7.0-7.1
+		    memcpy((u32*)0x080521C4, patchO0, 4);
+		    memcpy((u32*)0x08057E98, patchO1, 4);
+    		break;
+    	case '7': // 7.2
+		    memcpy((u32*)0x080521C8, patchO0, 4);
+		    memcpy((u32*)0x08057E9C, patchO1, 4);
+    		break;
+    	case '8': // 8.x
+		    memcpy((u32*)0x080523C4, patchO0, 4);
+		    memcpy((u32*)0x08058098, patchO1, 4);
+    		break;
+    	case '9': // 9.x
+		    memcpy((u32*)0x0805235C, patchO0, 4);
+		    memcpy((u32*)0x08058100, patchO1, 4);
+    		break;
+        //New 3ds
+    	case 'a': // 8.x
+		    memcpy((u32*)0x08053114, patchN0, 4);
+		    memcpy((u32*)0x080587E0, patchN1, 4);
+    		break;
+    	case 'b': // 9.x
+		    memcpy((u32*)0x08052FD8, patchN2, 4);
+		    memcpy((u32*)0x08058804, patchN3, 4);
+    		break;
+    }
+	DrawDebug(1,"Apply patch for type %c...                  Done!", cfw_FWValue);
 }
-
-void arm9dumper()
-{
+// @breif  Dump ARM9 Ram to file.
+void CFW_ARM9Dumper(void) {
 	DrawDebug(1,"");
 	DrawDebug(1,"");
 	DrawDebug(1,"");
@@ -168,39 +142,33 @@ void arm9dumper()
 	DrawDebug(0,"Press A to DUMP, B to skip.");
 
 	u32 pad_state = HidWaitForInput();
-
-
 	if (pad_state & BUTTON_B) DrawDebug(1,"Skipping...");
-	else
-	{
-		u32 written = 0;
-		u32 total = 0;
-		u32 result = 0;
-		u32 num = 0;
-		void *addr = 0x08000000;
-		u32 size = 0x00100000;
-		const u32 sz_chunk = 0x10000;
+	else {
+		u32 bytesWritten = 0;
+		u32 currentWritten = 0;
+		u32 currentSize = 0;
+		void *dumpAddr = (void*)0x08000000;
+		u32 fullSize = 0x00100000;
+		const u32 chunkSize = 0x10000;
 
 		if (FSFileCreate("/3ds/PastaCFW/RAM.bin", true)) {
-			while (total < size) {
-				num = size - total < sz_chunk ? size - total : sz_chunk;
-				written = FSFileWrite((u8 *)addr + total, num, total);
-				if (written != num) break;
-				total += written;
-				DrawDebug(0,"Dumping:                         %07d/%d", total, size);
+			while (currentWritten < fullSize) {
+				currentSize = fullSize - currentWritten < chunkSize ? fullSize - currentWritten : chunkSize;
+				bytesWritten = FSFileWrite((u8 *)dumpAddr + currentWritten, currentSize, currentWritten);
+				if (bytesWritten != currentSize) break;
+				currentWritten += bytesWritten;
+				DrawDebug(0,"Dumping:                         %07d/%d", currentWritten, fullSize);
 			}
 			FSFileClose();
-			result = (size == total);
 		}
 		DrawDebug(1,"");
 		DrawDebug(1,"");
-		DrawDebug(1,"Dump %s! Press any key to boot CFW.", result ? "finished" : "failed");
+		DrawDebug(1,"Dump %s! Press any key to boot CFW.", (fullSize == currentWritten) ? "finished" : "failed");
 		HidWaitForInput();
 	}
 }
-
+// @breif  Main routine indeed.
 int main() {
-
 	//BOOT
 	DrawClearScreenAll();
 	DrawDebug(1,"--------------- PASTA CFW LOADER ---------------");
@@ -210,15 +178,13 @@ int main() {
 	DrawDebug(1,"Initializing FS...                         Done!");
 	DrawDebug(1,"");
 	DrawDebug(0,"Getting system information...");
-	getSystemVersion();
+	CFW_getSystemVersion();
 	DrawDebug(1,"Getting system information...              Done!");
 	DrawDebug(1,"");
-	DrawDebug(1,"Your system is %s", systemVersion);
+	DrawDebug(1,"Your system is %s", cfw_FWString);
 	DrawDebug(1,"");
-	bootCFW_SecondStage();
-	if (dump_arm9 == true)arm9dumper();
-
+	CFW_SecondStage();
+	if (dump_arm9 == true)CFW_ARM9Dumper();
 	// return control to FIRM ARM9 code (performs firmlaunch)
 	return 0;
 }
-
