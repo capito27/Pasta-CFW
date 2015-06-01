a517205d22197472384ff03455830fd6408262e8
luxsie <luxsie@163.com>
2015-06-01 12:32:55 +0800

    `platform.c` symbols renamed, comments added.

--- CFW_loader/include/platform.h
+++ CFW_loader/include/platform.h
@@ -3,14 +3,15 @@
 typedef enum {
     PLATFORM_3DS,
     PLATFORM_N3DS,
-} Platform;
+} Platform_UnitType;
 
+//[Unused] Generate the version value for the actual version.
 #define SYSTEM_VERSION(major, minor, revision) \
 	(((major)<<24)|((minor)<<16)|((revision)<<8))
 
 extern char *platform_FWStrings[];
 
-Platform Platform_CheckUnit();
+Platform_UnitType Platform_CheckUnit(void);
 //Un-implemented
 //u32 osGetFirmVersion();
 //u32 osGetKernelVersion();
--- CFW_loader/source/platform.c
+++ CFW_loader/source/platform.c
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "platform.h"
 
-#define CONFIG_PLATFORM_REG ((volatile u32*)0x10140FFC)
+#define PLATFORM_REG ((volatile u32*)0x10140FFC)
 
 // Strings of All supported firmware versions, currently.
 char *platform_FWStrings[] = {
@@ -18,14 +18,15 @@ char *platform_FWStrings[] = {
     "New 3DS V. 9.0 - 9.2"
 };
 
-Platform GetUnitPlatform()
-{
-    switch (*CONFIG_PLATFORM_REG) {
+// @breif  [Unused] Determine platform of the console.
+// @retval PLATFORM_N3DS for New3DS, and PLATFORM_3DS for Old3DS.
+// @note   Maybe modified to support more platforms
+Platform_UnitType Platform_CheckUnit(void) {
+    switch (*PLATFORM_REG) {
         case 7:
             return PLATFORM_N3DS;
         case 1:
         default:
             return PLATFORM_3DS;
     }
-
 }
