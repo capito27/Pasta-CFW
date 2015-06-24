acfc7ea5bb60461ca578811aec33bc87b42e6ee8
AlbertoSONIC <albertosonic9000@gmail.com>
2015-06-24 18:45:05 +0200

    Changed buttons for "shutdown" feature

--- CFW_loader/source/main.c
+++ CFW_loader/source/main.c
@@ -497,7 +497,7 @@ int main(void) {
 				else if (menu_idx == 4)CFW_ARM9Dumper(); //ARM9 RAM DUMPER
 				else if (menu_idx == 5)CFW_Settings(); //SETTINGS
 			}
-			else if (pad_state & BUTTON_DOWN && pad_state & BUTTON_B) //POWER OFF
+			else if (pad_state & BUTTON_START && pad_state & BUTTON_SELECT) //POWER OFF
 			{
 				i2cWriteRegister(I2C_DEV_MCU, 0x20, (u8)(1 << 0)); //As seen on 3dbrew
 				while (1);
